46c7a9ae4956464d955c1a280acfd71423ead5fe
田中龍亮 <r.t433102@gmail.com>
2020-06-03 23:03:47 +0900

    add loop

--- 2/guessing_game/src/main.rs
+++ 2/guessing_game/src/main.rs
@@ -1,13 +1,33 @@
+extern crate rand;
+
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
 
     println!("Please input your guess.");
+    let secret_number = rand::thread_rng().gen_range(1, 101);
 
-    let mut guess = String::new();
+    println!("The secret number is: {}", secret_number);
 
-    io::stdin().read_line(&mut guess).expect("Failed to read line");
+    loop {
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line"); // read_lineがio::Result型（実態はenum）を返し、Result型がErrのときクラッシュさせる
 
-    println!("you guessed: {}", guess);
+        println!("you guessed: {}", guess);
+        let guess: u32 = guess.trim().parse()
+            .expect("please type a number");
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("too small"),
+            Ordering::Greater => println!("too big"),
+            Ordering::Equal => {
+                println!("match");
+                break;
+            }
+        }
+    }
+
 }
