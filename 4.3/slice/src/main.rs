5fd44d6cbed7b0c8603b4c11a370daca7aa0a6a1
田中龍亮 <r.t433102@gmail.com>
2020-06-06 21:19:54 +0900

    add 4.3

---
+++ 4.3/slice/src/main.rs
@@ -0,0 +1,26 @@
+fn first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
+}
+fn main() {
+    let my_string = String::from("hello world");
+
+    // first_wordは`String`のスライスに対して機能する
+    let word = first_word(&my_string[..]);
+
+    let my_string_literal = "hello world";
+
+    // first_wordは文字列リテラルのスライスに対して機能する
+    let word = first_word(&my_string_literal[..]);
+
+    // 文字列リテラルは、すでに文字列スライス*な*ので、
+    // スライス記法なしでも機能するのだ！
+    let word = first_word(my_string_literal);
+}
